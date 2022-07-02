c78cd3bb4866fc06206ac46a97044e4fbb2e6f67
moyuh <mlroth94@gmail..com>
2022-07-01 23:01:18 -0600

    adding constants

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numeric = "0123456789";
+const specialCharacters = "!@#$%^&*_-=+";
 
 function generatePassword() {
+
   
+  
+  // return a value to the password box
+  return password;
 }
   
 // Write password to the #password input
